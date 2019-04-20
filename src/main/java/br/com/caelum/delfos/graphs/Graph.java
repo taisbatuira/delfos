0b6b47bd462b2e6d316c2d34e8d40f577f860189
Yuri Matheus <yurimatheusb@hotmail.com>
2019-04-19 22:07:07 -0300

    turns into interface

--- src/main/java/br/com/caelum/delfos/graphs/Graph.java
+++ src/main/java/br/com/caelum/delfos/graphs/Graph.java
@@ -1,4 +1,13 @@
 package br.com.caelum.delfos.graphs;
 
-public class Graph {
+import java.util.Optional;
+import java.util.Set;
+
+public interface Graph<V, E> {
+
+    Set<V> getVertexes();
+
+    org.jgrapht.Graph<V, E> getGraphT();
+
+    Optional<V> getVertex(V vertex);
 }
