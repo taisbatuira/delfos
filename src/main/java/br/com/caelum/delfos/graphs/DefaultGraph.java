86d56c878b9f7bcf98a95178c418ac11ac6f60a4
Yuri Matheus <yurimatheusb@hotmail.com>
2019-04-19 22:07:47 -0300

    creates the Default directed graph

---
+++ src/main/java/br/com/caelum/delfos/graphs/DefaultGraph.java
@@ -0,0 +1,37 @@
+package br.com.caelum.delfos.graphs;
+
+import br.com.caelum.delfos.graphs.exceptions.GraphException;
+import org.jgrapht.graph.DefaultEdge;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class DefaultGraph implements Graph<Integer, DefaultEdge> {
+
+
+    private final org.jgrapht.Graph<Integer, DefaultEdge> graph;
+
+    public DefaultGraph(org.jgrapht.Graph<Integer, DefaultEdge> graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public Set<Integer> getVertexes() {
+        return graph.vertexSet();
+    }
+
+    @Override
+    public org.jgrapht.Graph<Integer, DefaultEdge> getGraphT() {
+        return graph;
+    }
+
+    @Override
+    public Optional<Integer> getVertex(Integer vertex) {
+        return  graph.vertexSet()
+                     .stream()
+                     .filter(v -> v.equals(vertex))
+                     .findAny();
+
+    }
+
+}
